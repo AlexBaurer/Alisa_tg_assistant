f970357d4c5713b6e61394a84e2b7fb623041bff
zhukov_i <HwG37YDZXaa2kcBAmVZi>
2023-02-20 02:29:53 +0300

    feat: big refactor, add authenticators protocol, add entrypoint

--- controller.py
+++ controller.py
@@ -1,20 +1,19 @@
-import asyncio
-from tg_client import create_client
+from clients.telegram import TelegramClient, create_client
 from client_db import create_user
 
 
 class Controller:
-
     def __init__(self):
         self.phone_code = None
-        self.client = None
+        self.client: TelegramClient | None = None
 
     async def phone_handle(self, user_phone):
         self.client = await create_client(user_phone)
         create_user(self.client, self.client.get_me(), )
 
     async def code_handle(self, phone_code):
-        await self.client.sign_in_by_code(phone_code)
+        if self.client:
+            await self.client.sign_in_by_code(phone_code)
 
     async def alisa_request_handler(self, request):
         if request:
